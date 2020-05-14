bbec51348cd749e2877c1550f54d3883b520282a
biteyou <51965912+biteyou@users.noreply.github.com>
2020-05-14 21:12:00 +0800

    Add files via upload

---
+++ package_32_no_cmd.cmd
@@ -0,0 +1,5 @@
+E
+cd E:\my python scipt\osreadpdf
+#chcp 65001
+C:\Python37-32\Scripts\pyinstaller.exe --paths C:\Python37-32\Lib\site-packages\PyQt5\Qt\bin -F -w osreadpdf_log.py
+ping www.baidu.com
